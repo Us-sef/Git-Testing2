230846a03a39359b1ccd69938a3a63c8ebdf2ea0
Us-sef <73020576+Us-sef@users.noreply.github.com>
2023-03-12 22:09:04 +0100

    Update Main.java
    update name of person1 in Main class

--- src/firstProject/Main.java
+++ src/firstProject/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Person p1 = new Person("youssef");
+		Person p1 = new Person("youssef55");
 		p1.setLname("fhal");
 		p1.setAge(23);
 		p1.getAge();
