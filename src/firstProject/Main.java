51749de257798bd22a312783fb84b6699551a7bb
Us-sef <youssef.fhal@etu.ac.ma>
2023-03-12 22:03:16 +0100

    project now is tracked by github

---
+++ src/firstProject/Main.java
@@ -0,0 +1,23 @@
+package firstProject;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Person p1 = new Person("youssef");
+		p1.setLname("fhal");
+		p1.setAge(23);
+		p1.getAge();
+		
+		Person p2 = new Person("hamza");
+		p2.setLname("el kaich");
+		p2.setAge(23);
+		
+		
+		System.out.println(p1);
+		System.out.println(p2);
+	
+	}
+
+
+}
---
+++ src/firstProject/Person.java
@@ -0,0 +1,46 @@
+package firstProject;
+
+public class Person {
+	
+	String fname ; 
+	String lname ; 
+	int age ;
+	
+	public Person(String fname) {
+		this.fname = fname;
+	}
+	
+	
+	@Override
+	public String toString() {
+		return "Person [fname=" + fname + ", lname=" + lname + ", age=" + age + "]";
+	}
+
+
+	public Person(String fname, String lname, int age) {
+		this.fname = fname;
+		this.lname = lname;
+		this.age = age;
+	}
+	
+	public String getFname() {
+		return fname;
+	}
+	public void setFname(String fname) {
+		this.fname = fname;
+	}
+	public String getLname() {
+		return lname;
+	}
+	public void setLname(String lname) {
+		this.lname = lname;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	} 
+	
+
+}
